0a69b7bd5710189aef90eb6e3d6aed910eddac75
wangb <wangbinyq@gmail.com>
2025-08-27 12:14:14 +0800

    fix(orbitcamera): improve camera collision detection with boxcast adjustments
    Refactor the boxcast logic to use near clip plane offset for more accurate collision detection. This change adjusts the cast origin and direction based on the camera's near clip plane, ensuring the camera position is correctly calculated when collisions occur.

--- Assets/Scripts/OrbitCamera.cs
+++ Assets/Scripts/OrbitCamera.cs
@@ -58,13 +58,20 @@ public class OrbitCamera : MonoBehaviour
         Vector3 lookDirection = lookRotation * Vector3.forward;
         Vector3 lookPosition = focusPoint - lookDirection * distance;
 
+        Vector3 rectOffset = lookDirection * regularCamera.nearClipPlane;
+        Vector3 rectPosition = lookPosition + rectOffset;
+        Vector3 castFrom = focus.position;
+        Vector3 castLine = rectPosition - castFrom;
+        float castDistance = castLine.magnitude;
+        Vector3 castDirection = castLine / castDistance;
+
         if (Physics.BoxCast(
-            focusPoint, CameraHalfExtends, -lookDirection, out RaycastHit hit,
-            lookRotation, distance - regularCamera.nearClipPlane
+            castFrom, CameraHalfExtends, castDirection, out RaycastHit hit,
+            lookRotation, castDistance
         ))
         {
-            lookPosition = focusPoint -
-                lookDirection * (hit.distance + regularCamera.nearClipPlane);
+            rectPosition = castFrom + castDirection * hit.distance;
+            lookPosition = rectPosition - rectOffset;
         }
 
         transform.SetPositionAndRotation(lookPosition, lookRotation);
