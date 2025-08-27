2530d3256f47adb26a9d75c77578d28096749177
wangb <wangbinyq@gmail.com>
2025-08-27 11:41:46 +0800

    feat(orbitcamera): add focus radius smoothing and update camera logic
    - Introduced a focus radius parameter to smooth camera movement around a target
    - Refactored LateUpdate to use a new UpdateFocusPoint method for better control
    - Added Awake method to initialize focus point
    - Improved camera interpolation by lerping between target and current focus point when outside the focus radius
    - Updated Rigidbody interpolation setting in SampleScene.unity for smoother physics simulation
    - Added maxStairsAngle and stairsMask parameters to MovingSphere ground detection logic

--- Assets/Scripts/OrbitCamera.cs
+++ Assets/Scripts/OrbitCamera.cs
@@ -7,11 +7,37 @@ public class OrbitCamera : MonoBehaviour
 
     [SerializeField, Range(1f, 20f)]
     float distance = 5f;
+    [SerializeField, Min(0f)]
+    float focusRadius = 1f;
+
+    Vector3 focusPoint;
+
+    void Awake()
+    {
+        focusPoint = focus.position;
+    }
 
     void LateUpdate()
     {
-        Vector3 focusPoint = focus.position;
+        UpdateFocusPoint();
         Vector3 lookDirection = transform.forward;
         transform.localPosition = focusPoint - lookDirection * distance;
+    }
+
+    void UpdateFocusPoint()
+    {
+        var targetPoint = focus.position;
+        if (focusRadius > 0f)
+        {
+            var distance = Vector3.Distance(targetPoint, focusPoint);
+            if (distance > focusRadius)
+            {
+                focusPoint = Vector3.Lerp(targetPoint, focusPoint, focusRadius / distance);
+            }
+        }
+        else
+        {
+            focusPoint = targetPoint;
+        }
     }
 }
